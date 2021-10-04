b463df14d1c5ca488d50279f409bdd906df2335f
Jon Suderman <jon@suderman.net>
2021-10-04 19:05:37 +0000

    cache

--- config/application.php
+++ config/application.php
@@ -12,30 +12,16 @@ use Roots\WPConfig\Config;
 use function Env\env;
 
 // Directory containing all of the site's files
-$root_dir = dirname(__DIR__);
-
-// Document Root
-$webroot_dir = $root_dir . '/web';
-
+Config::define( 'ROOT_DIR', dirname(__DIR__) );
 
 // Use Dotenv to set required environment variables and load .env file in root
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir);
-if (file_exists($root_dir . '/.env')) {
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable( Config::get('ROOT_DIR') );
+if (file_exists(Config::get('ROOT_DIR') . '/.env')) {
   $dotenv->load();
 }
 
-// Swarm host primary
-$HOST = strtolower(env('HOST'));
-Config::define('HOST', $HOST);
-$HOST_SLUG = str_replace( [' ', '-', '.'], '_', $HOST );
-
-$APP = strtok($HOST, '.');
-Config::define('APP', $APP);
-$APP_SLUG = str_replace( [' ', '-', '.'], '_', $APP );
-
 // Set up our global environment constant and load its config first
-// Default: production
-define('WP_ENV', env('WP_ENV') ?: 'production');
+Config::define( 'WP_ENV', env('WP_ENV') ?: 'production' );
 
 // Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
 // See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
@@ -43,88 +29,69 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
   $_SERVER['HTTPS'] = 'on';
 }
 
+// Swarm host primary
+Config::define( 'HOST', strtolower(env('HOST')) );
+Config::define( 'HOST_SLUG', str_replace([' ', '-', '.'], '_', Config::get('HOST')) );
+
+// App name
+Config::define( 'APP', env('APP') ?: strtok(Config::get('HOST'), '.') );
+Config::define( 'APP_SLUG', str_replace([' ', '-', '.'], '_', Config::get('APP')) );
+
 // URLs
-if (env('WP_HOME')) {
-  Config::define('WP_HOME', env('WP_HOME'));
-  Config::define('WP_SITEURL', env('WP_SITEURL') ?: env('WP_HOME') . '/wp');
-} else {
-  $http_or_https = ( isset( $_SERVER['HTTPS'] ) ) ? 'https' : 'http';
-  Config::define('WP_HOME', "${http_or_https}://" . env('HTTP_HOST'));
-  Config::define('WP_SITEURL', "${http_or_https}://" . env('HTTP_HOST') . '/wp');
-}
+Config::define( 'WP_HOME', env('WP_HOME') ?: ( (isset($_SERVER['HTTPS'])) ? 'https://' : 'http://' ) . env('HTTP_HOST') );
+Config::define( 'WP_SITEURL', env('WP_SITEURL') ?: Config::get('WP_HOME') . '/wp' );
 
 // Custom Content Directory
-Config::define('ROOT_DIR', $root_dir);
-Config::define('WEBROOT_DIR', $webroot_dir);
-Config::define('CONTENT_DIR', '/content');
-Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
-Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
+Config::define( 'WEBROOT_DIR', Config::get('ROOT_DIR') . '/web' );
+Config::define( 'CONTENT_DIR', '/content' );
+Config::define( 'WP_CONTENT_DIR', Config::get('WEBROOT_DIR') . Config::get('CONTENT_DIR'));
+Config::define( 'WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
 // DB settings
-$DB_NAME_ENV = strtoupper('DB_NAME_'.WP_ENV);
-Config::define('DB_NAME', env('DB_NAME') ?: (env($DB_NAME_ENV) ?: $HOST_SLUG));
-Config::define('DB_USER', env('DB_USER') ?: $APP_SLUG);
-Config::define('DB_PASSWORD', env('DB_PASSWORD'));
-Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
-Config::define('DB_CHARSET', 'utf8mb4');
-Config::define('DB_COLLATE', '');
+Config::define( 'DB_NAME', env('DB_NAME') ?: (env(strtoupper('DB_NAME_'.Config::get('WP_ENV'))) ?: Config::get('HOST_SLUG')) );
+Config::define( 'DB_USER', env('DB_USER') ?: Config::get('APP_SLUG') );
+Config::define( 'DB_PASSWORD', env('DB_PASSWORD') );
+Config::define( 'DB_HOST', env('DB_HOST') ?: 'localhost' );
+Config::define( 'DB_CHARSET', 'utf8mb4' );
+Config::define( 'DB_COLLATE', '' );
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
 
-
 // Reddis Cache settings
-// global $redis_server;
 $redis_server = [ 'host' => 'redis' ];
+
+// Enable caching
+Config::define( 'WP_CACHE', env('WP_CACHE') ?: true );
 
 // Authentication Unique Keys and Salts
 foreach([ 
   'AUTH_KEY', 'SECURE_AUTH_KEY', 'LOGGED_IN_KEY', 'NONCE_KEY', 
   'AUTH_SALT', 'SECURE_AUTH_SALT', 'LOGGED_IN_SALT', 'NONCE_SALT', 
 ] as $key) {
-  
-  // If a key defined in the env, use that value
-  if ( env($key) ) {
-    Config::define( $key, env($key) );
-
-  // Otherwise, generate a value
-  } else {
-    Config::define( $key, hash('sha256', $key . $HOST . env('DB_PASSWORD') ) );
-  }
+  // If a key defined in the env, use that value. Otherwise, generate a value
+  Config::define( $key, env($key) ?: hash('sha256', $key . Config::get('HOST') . Config::get('DB_PASSWORD')) );
 }
 
 // Custom Settings
-Config::define('AUTOMATIC_UPDATER_DISABLED', true);
-Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: true);
-// Disable the plugin and theme file editor in the admin
-Config::define('DISALLOW_FILE_EDIT', true);
-// Disable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', true);
+Config::define( 'AUTOMATIC_UPDATER_DISABLED', env('AUTOMATIC_UPDATER_DISABLED') ?: true );
+Config::define( 'DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: true );
+Config::define( 'DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT') ?: true ); // Disable the plugin and theme file editor in the admin
+Config::define( 'DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS') ?: true ); // Disable plugin and theme updates and installation from the admin
+Config::define( 'FS_METHOD', env('FS_METHOD') ?: 'direct' );
 
 // Query Monitor Settings
-Config::define('QM_DARK_MODE', env('QM_DARK_MODE') ?: true);
-Config::define('QM_ENABLE_CAPS_PANEL', env('QM_ENABLE_CAPS_PANEL') ?: true);
-Config::define('QM_DB_SYMLINK', env('QM_DB_SYMLINK') ?: false);
-
-// https://querymonitor.com/blog/2018/07/silencing-errors-from-plugins-and-themes/
-Config::define('QM_DISABLE_ERROR_HANDLER', env('QM_DISABLE_ERROR_HANDLER') ?: false);
-
-// Enable caching
-Config::define('WP_CACHE', env('WP_CACHE') ?: true);
-Config::define('FS_METHOD', 'direct');
+Config::define( 'QM_DARK_MODE', env('QM_DARK_MODE') ?: true );
+Config::define( 'QM_ENABLE_CAPS_PANEL', env('QM_ENABLE_CAPS_PANEL') ?: true );
+Config::define( 'QM_DB_SYMLINK', env('QM_DB_SYMLINK') ?: false );
+Config::define( 'QM_DISABLE_ERROR_HANDLER', env('QM_DISABLE_ERROR_HANDLER') ?: false ); // https://querymonitor.com/blog/2018/07/silencing-errors-from-plugins-and-themes/
 
 // Debugging Settings
-Config::define('WP_DEBUG_DISPLAY', false);
-Config::define('SCRIPT_DEBUG', false);
-ini_set('display_errors', '0');
+Config::define( 'WP_DEBUG_DISPLAY', false );
+Config::define( 'SCRIPT_DEBUG', false );
+ini_set( 'display_errors', '0' );
 
 // Environment configs
-$env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
-if (file_exists($env_config)) {
-  require_once $env_config;
+if ( file_exists(__DIR__ . '/environments/' . Config::get('WP_ENV') . '.php') ) {
+  require_once __DIR__ . '/environments/' . Config::get('WP_ENV') . '.php';
 }
 
 Config::apply();
-
-// Bootstrap WordPress
-if (!defined('ABSPATH')) {
-  define('ABSPATH', $webroot_dir . '/wp/');
-}
--- web/content/advanced-cache.php
+++ web/content/advanced-cache.php
@@ -14,6 +14,78 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
+
+// Tweaked dropin for Cache Enabler
+// Helps clear local disk cache on all nodes in docker swarm
+//
+function on_cache_clear( $clear = [] ) {
+  if ( is_user_logged_in() ) {
+
+    // Clear out the cache file
+    $cache_dir = dirname( __file__ ) . '/cache/';
+    array_map( 'unlink', glob( $cache_dir . 'nf_cache_*' ) );
+
+    // Also clear out the cached settings file
+    $settings_dir = dirname( __file__ ) . '/settings/cache-enabler/';
+    array_map( 'unlink', glob( $settings_dir . '*.php' ) );
+
+    // Save the current cache clear instructions
+    $nf_cache = [ 'file' => 'nf_cache_'.time(), 'clear' => $clear, ];
+    set_transient('nf_cache', $nf_cache); 
+    touch( $cache_dir . $nf_cache['file'] );
+
+  }
+}
+
+// Hooks triggered when cache cleared
+add_action( 'cache_enabler_page_cache_cleared', function( $url, $id, $index ) {
+  on_cache_clear([ 'url' => $url, 'post' => $id ]);
+}, 10, 3 );
+
+
+add_action( 'cache_enabler_site_cache_cleared', function( $url, $id, $index ) {
+  on_cache_clear([ 'site' => $id ]);
+}, 10, 3 );
+
+
+add_action( 'cache_enabler_complete_cache_cleared', function() {
+  on_cache_clear([ 'full' => true ]);
+}, 10, 3);
+
+
+// Try to clear cache on every page load if necessary
+// Check transient (redis) if there's clearing to do
+add_action('init', function() {
+
+  $cache_dir = dirname( __file__ ) . '/cache/';
+  $file = get_transient('nf_cache')['file'] ?? 'nf_cache_new'; 
+
+  if ( ! file_exists( $cache_dir . $file ) ) {
+
+    foreach( get_transient('nf_cache')['clear'] ?? [] as $type => $arg ) {
+
+      if ( $type == 'url' ) {
+        do_action( 'cache_enabler_clear_page_cache_by_url', $arg );
+
+      } elseif ( $type == 'post' ) {
+        do_action( 'cache_enabler_clear_page_cache_by_post', $arg );
+      
+      } elseif ( $type == 'site' ) {
+        do_action( 'cache_enabler_clear_site_cache', $arg );
+
+      } elseif ( $type == 'full' ) {
+        do_action( 'cache_enabler_clear_complete_cache' );
+      }
+
+    }
+
+    touch( $cache_dir . $file );
+
+  }
+
+});
+
+
 $cache_enabler_dir = dirname( __FILE__ ) . '/plugins/cache-enabler';
 $cache_enabler_constants_file = $cache_enabler_dir . '/constants.php';
 
