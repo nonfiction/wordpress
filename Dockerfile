050b7e94b6e3b6124839f0902e46ed7e5d478abf
Jon Suderman <jon@suderman.net>
2022-03-10 18:04:13 +0000

    healthcheck + zero downtime

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nonfiction/wordpress-base:v1
+FROM nonfiction/wordpress-base:v2
 
 # Copy the codebase
 COPY ./app /srv/app
@@ -7,8 +7,20 @@ COPY ./src /srv/src
 COPY ./theme /srv/theme
 COPY ./vendor /srv/vendor
 COPY ./web /srv/web
-RUN ln -sf /srv/theme /srv/web/content/themes/theme
 COPY ./composer.json /srv/composer.json
+
+# Symlink PageSpeed config
+RUN ln -sf /srv/config/pagespeed.conf /etc/apache2/mods-enabled/pagespeed.conf
+
+# Symlink theme folder to where it belongs
+RUN ln -sf /srv/theme /srv/web/content/themes/theme
+
+# Assets in the /srv/app/views/img directory are web public at /assets/img
+RUN ln -sf /srv/app/views/img /srv/web/assets/img
+
+# Workarounds so resources can still be found when looking in the wrong place
+RUN ln -sf /srv/web/content /srv/web/wp-content
+RUN ln -sf /srv/web/app /srv/web/content
 
 # Persist uploads in this volume
 VOLUME /srv/web/content/uploads
--- run.sh
+++ run.sh
@@ -15,31 +15,18 @@ export SMTP_PASSWORD
 # Update msmtp config for transactional email
 esh /etc/msmtprc.esh > /etc/msmtprc
 
-# Symlink PageSpeed config
-ln -sf /srv/config/pagespeed.conf /etc/apache2/mods-enabled/pagespeed.conf
-
-# Symlink theme folder to where it belongs
-rm -rf /srv/web/content/themes/theme && ln -sf /srv/theme /srv/web/content/themes/theme
-
 # Protect drop-in plugins from being modified
 chown root:root /srv/web/content/object-cache.php && chmod -w /srv/web/content/object-cache.php
 chown root:root /srv/web/content/advanced-cache.php && chmod -w /srv/web/content/advanced-cache.php
 chown root:root /srv/web/content/db.php && chmod -w /srv/web/content/db.php
 
+# Make sure upload/cache/settings directories are writeable
+chown -R www-data:www-data /srv/web/content/uploads /srv/web/content/cache /srv/web/content/settings
+
 # Ensure the cache is cleared
 rm -rf /srv/web/content/cache/* 
 rm -rf /srv/web/content/settings/* 
 /usr/local/bin/wp cache flush
-
-# Make sure upload/cache/settings directories are writeable
-chown -R www-data:www-data /srv/web/content/uploads /srv/web/content/cache /srv/web/content/settings
-
-# Assets in the /srv/app/views/img directory are web public at /assets/img
-rm -f /srv/web/assets/img && ln -sf /srv/app/views/img /srv/web/assets/img
-
-# Workarounds so resources can still be found when looking in the wrong place
-rm -f /srv/web/wp-content && ln -sf /srv/web/content /srv/web/wp-content
-rm -f /srv/web/app && ln -sf /srv/web/app /srv/web/content
 
 # Run Apache in foregrond
 apachectl -D FOREGROUND
